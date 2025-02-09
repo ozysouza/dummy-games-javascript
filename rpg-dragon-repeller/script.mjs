22033eabbf83bc3435a43c3d7776c1bb77a92542
ozysouza <ozysouzaa@gmail.com>
2025-02-09 17:16:49 -0500

    Add player starting stats on the game

---
+++ rpg-dragon-repeller/script.mjs
@@ -0,0 +1,10 @@
+// This script controls the game's logic, including character stats, navigation, combat, and inventory.
+
+// Player's starting stats
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let invetory = ["stick"];
